683149982e71c410f52848b95d537a055b67afa3
divisor <div@altertech.com>
2019-07-19 23:01:45 +0200

    0.2.2

--- atasker/__init__.py
+++ atasker/__init__.py
@@ -1,7 +1,7 @@
 __author__ = "Altertech Group, http://www.altertech.com/"
 __copyright__ = "Copyright (C) 2018-2019 Altertech Group"
 __license__ = "Apache License 2.0"
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 from atasker.supervisor import TaskSupervisor
 from atasker.supervisor import TASK_LOW
