7d86b37d040a957deb0f3adf64af7394b43c552e
divisor <div@altertech.com>
2019-08-22 23:41:27 +0200

    0.3.5

--- atasker/supervisor.py
+++ atasker/supervisor.py
@@ -1,7 +1,7 @@
 __author__ = "Altertech Group, https://www.altertech.com/"
 __copyright__ = "Copyright (C) 2018-2019 Altertech Group"
 __license__ = "Apache License 2.0"
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 
 import threading
 import multiprocessing
@@ -281,8 +281,8 @@ class TaskSupervisor:
             return len(self._active_mps)
 
     def create_aloop(self, name, daemon=False, start=True, default=False):
-        if name == 'supervisor':
-            raise RuntimeError('Name "supervisor" is reserved')
+        if name == '__supervisor__':
+            raise RuntimeError('Name "__supervisor__" is reserved')
         with self._lock:
             if name in self.aloops:
                 logger.error('loop {} already exists'.format(name))
