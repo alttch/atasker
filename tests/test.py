25dfaa01dbac8989edd8972231f4c89f58f92b31
divisor <div@altertech.com>
2019-08-29 18:11:20 +0200

    0.3.20

--- tests/test.py
+++ tests/test.py
@@ -3,7 +3,7 @@
 __author__ = "Altertech Group, https://www.altertech.com/"
 __copyright__ = "Copyright (C) 2018-2019 Altertech Group"
 __license__ = "Apache License 2.0"
-__version__ = "0.3.16"
+__version__ = "0.3.20"
 
 from pathlib import Path
 
