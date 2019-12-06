03b7a5cf9be3a1ab2cc18bf3c639a3cf5da51725
Sergei S <div@altertech.com>
2019-12-06 15:04:31 +0000

    0.5.0

--- atasker/workers.py
+++ atasker/workers.py
@@ -1,7 +1,7 @@
 __author__ = 'Altertech Group, https://www.altertech.com/'
 __copyright__ = 'Copyright (C) 2018-2019 Altertech Group'
 __license__ = 'Apache License 2.0'
-__version__ = "0.4.7"
+__version__ = "0.5.0"
 
 import threading
 import logging
