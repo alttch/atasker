5711e77047135e5fad6e87e102191a754b9aed22
Sergei S <div@altertech.com>
2019-12-07 00:45:32 +0000

    0.5.1

--- atasker/co.py
+++ atasker/co.py
@@ -1,7 +1,7 @@
 __author__ = 'Altertech Group, https://www.altertech.com/'
 __copyright__ = 'Copyright (C) 2018-2019 Altertech Group'
 __license__ = 'Apache License 2.0'
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 from atasker import task_supervisor
 
