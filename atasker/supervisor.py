6503abd450b3480f5bdfa6ebfb2ea6f3dee8838b
Sergei S <div@altertech.com>
2019-12-09 03:13:46 +0000

    0.7.3

--- atasker/supervisor.py
+++ atasker/supervisor.py
@@ -1,7 +1,7 @@
 __author__ = 'Altertech Group, https://www.altertech.com/'
 __copyright__ = 'Copyright (C) 2018-2019 Altertech Group'
 __license__ = 'Apache License 2.0'
-__version__ = "0.7.2"
+__version__ = "0.7.3"
 
 import threading
 import multiprocessing
